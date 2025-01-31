ab384bb93cd692399ea4f5ddfad06f861405cf56
romeu.comastri <comastriromeu@gmail.com>
2025-01-31 14:40:49 -0300

    Alteração css editar aluno

--- src/ProtectedRoute.js
+++ src/ProtectedRoute.js
@@ -5,12 +5,9 @@ import UsuarioApi from './services/UsuarioApi';
 
 
 const ProtectedRoute = ({ children }) => {
-    console.log("Verificando autenticação...");
     const usuarioId = localStorage.getItem('usuarioId');
-    console.log("Usuário ID:", usuarioId);
     
     if (usuarioId === null) {
-        console.log("Usuário não autenticado. Redirecionando...");
         return <Navigate to="/login" />;
     }
 
